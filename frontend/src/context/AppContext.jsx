4c1466f0b3a85cc4a09e88d4bd667c22f7639d23
Dhanesh Joshi <115444628+DhaneshJoshi842@users.noreply.github.com>
2025-03-03 23:31:40 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
     const currencySymbol = '₹';
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = https://projectprescripto-backend.onrender.com;
 
     const [doctors, setDoctors] = useState([]);
     const [token, setToken] = useState(null);  // Initial state null
